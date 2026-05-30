dc58c7fff2f20e9d4fbdb3e9013111d133d04809
andresdanielmtz <autummata@gmail.com>
2026-05-29 20:16:58 -0600

    add: basic parser setup, inclouiding dev command

---
+++ src/parser/constants.ts
@@ -0,0 +1,27 @@
+/**
+ * This file defines the various token types that the lexer will recognize in the source code.
+ * todo: it might be a good idea to join the constants between the lexer and the parser into a single file or folder; but that would require to restructure the project a bit, so for now we can keep them separate and just make sure to keep them in sync as we develop.
+ */
+enum TOKEN_TYPE {
+    IDENTIFIER = "IDENTIFIER",
+    NUMBER = "NUMBER",
+    STRING = "STRING",
+    OPERATOR = "OPERATOR",
+    KEYWORD = "KEYWORD",
+    DELIMITER = "DELIMITER",
+    NEWLINE = "NEWLINE",
+    PUNCTUATION = "PUNCTUATION",
+    COMMENT = "COMMENT",
+    WHITESPACE = "WHITESPACE",
+    EOF = "EOF"
+}
+
+interface Token {
+    type: TOKEN_TYPE;
+    value: string;
+    line: number;
+    column: number;
+}
+
+export { TOKEN_TYPE };
+export type { Token };
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,1 +1,15 @@
-export {};
+import * as fs from "fs";
+import * as path from "path";
+import { Token } from "./constants"
+
+const readTokens = (): Token[] => { 
+    const OUTPUT_DIRECTORY = path.resolve(__dirname, "../../output");
+    const TOKENS_FILE = path.join(OUTPUT_DIRECTORY, "tokens.json");
+    const tokensData = fs.readFileSync(TOKENS_FILE, "utf-8");
+    const tokens = JSON.parse(tokensData);
+    return tokens;
+}
+
+// For now, just print the tokens
+const tokenList = readTokens();
+console.log(tokenList);
